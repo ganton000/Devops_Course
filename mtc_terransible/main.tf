c3c135171a10c2fd0174ce06aa1e710cf9ffd566
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-02-11 14:56:42 -0500

    Added random_id interpolation to tag names for unique resource names

--- mtc_terransible/main.tf
+++ mtc_terransible/main.tf
@@ -1,9 +1,21 @@
+resource "random_id" "random" {
+	byte_length = 2
+}
+
 resource "aws_vpc" "terraform_vpc" {
 	cidr_block = var.vpc_cidr
 	enable_dns_hostnames = true
 	enable_dns_support = true
 
 	tags = {
-		Name = "terraform_vpc"
+		Name = "terraform_vpc-${random_id.random.dec}"
+	}
+}
+
+resource "aws_internet_gateway" "terraform_internet_gateway" {
+	vpc_id = aws_vpc.terraform_vpc.id
+
+	tags = {
+		Name = "terraform_igw-${random_id.random.dec}"
 	}
 }
