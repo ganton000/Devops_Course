07c8554b6b4313ab72ec51e98805030f5e9ba41d
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-02-11 15:20:38 -0500

    Added route table and route resource

--- mtc_terransible/main.tf
+++ mtc_terransible/main.tf
@@ -23,3 +23,18 @@ resource "aws_internet_gateway" "terraform_internet_gateway" {
 		Name = "terraform_igw-${random_id.random.dec}"
 	}
 }
+
+resource "aws_route_table" "terraform_public_rt" {
+	vpc_id = aws_vpc.terraform_vpc.id
+
+	tags = {
+		Name = "terraform-public"
+	}
+}
+
+resource "aws_route" "terraform_route" {
+	route_table_id = aws_route_table.terraform_public_rt.id
+	destination_cidr_block = "0.0.0.0/0"
+	gateway_id = aws_internet_gateway.terraform_internet_gateway.id
+
+}
