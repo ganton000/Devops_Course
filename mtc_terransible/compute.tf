5f3a2aafa2600b7f64560b25f8b4dfa2974565c6
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-02-12 21:34:10 -0500

    Added playbook to compute.tf

--- mtc_terransible/compute.tf
+++ mtc_terransible/compute.tf
@@ -30,9 +30,9 @@ resource "aws_instance" "terraform_main" {
   key_name               = aws_key_pair.terraform_auth.id #can also use .key_name
   vpc_security_group_ids = [aws_security_group.terraform_sg.id]
   subnet_id              = aws_subnet.terraform_public_subnet[count.index].id
-  user_data = templatefile("./main-userdata.tpl", {
-    new_hostname = "terraform-main-${random_id.ec2_random_id[count.index].dec}"
-  })
+  #user_data = templatefile("./main-userdata.tpl", {
+  #  new_hostname = "terraform-main-${random_id.ec2_random_id[count.index].dec}"
+  #})
 
   root_block_device {
     volume_size = var.main_vol_size # 8 gibibytes (GiB)
@@ -47,7 +47,7 @@ resource "aws_instance" "terraform_main" {
   ## self keyword within provisioner references the resource it is in
   ## can specifiy interpreter as well
   provisioner "local-exec" {
-    command = "printf \"\n${self.public_ip}\" >> aws_hosts"
+    command = "printf \"\n${self.public_ip}\" >> aws_hosts && aws ec2 wait instance-status-ok --instance-ids {self.id} --region us-east-1 --profile admin"
   }
 
   ## destroy provisioner -- acts as cleanup
@@ -63,18 +63,30 @@ resource "aws_instance" "terraform_main" {
 # since it modifies our above aws_instance (ec2 instance resource), then
 # must be created outside of that resource block
 # null_resource is a placeholder for an empty resource
-resource "null_resource" "grafana_update" {
+#resource "null_resource" "grafana_update" {
 
-  count = var.ec2_instance_count
-  provisioner "remote-exec" {
+#  count = var.ec2_instance_count
+#  provisioner "remote-exec" {
 
-    inline = ["sudo apt upgrade -y grafana && touch upgrade.log && echo 'I updated Grafana' >> upgrade.log"]
+#    inline = ["sudo apt upgrade -y grafana && touch upgrade.log && echo 'I updated Grafana' >> upgrade.log"]
 
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.ssh/terrakey")
-      host        = aws_instance.terraform_main[count.index].public_ip
-    }
+#    connection {
+#      type        = "ssh"
+#      user        = "ubuntu"
+#      private_key = file("~/.ssh/terrakey")
+#      host        = aws_instance.terraform_main[count.index].public_ip
+#    }
+#  }
+#}
+
+## use ansible playbook instead of user_data for instances
+resource "null_resource" "grafana_install" {
+  depends_on = [aws_instance.terraform_main] # instances need to exist for
+
+  provisioner "local-exec" {
+    # -i specifies custom inventory file
+    #
+    command = "ansible-playbook -i aws_hosts --key-file ~/.ssh/terrakey playbooks/grafana.yml"
+
   }
 }
